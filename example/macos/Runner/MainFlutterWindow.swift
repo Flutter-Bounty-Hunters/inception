829d56d1107469b48e1afc3e4f4fe4b65b6238ab
Matt Carroll <matthew-carroll@users.noreply.github.com>
2024-09-25 23:09:04 -0700

    Hide title bar at the top of Mac window

--- example/macos/Runner/MainFlutterWindow.swift
+++ example/macos/Runner/MainFlutterWindow.swift
@@ -11,5 +11,14 @@ class MainFlutterWindow: NSWindow {
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
+
+    // Configure initial window size and styles, including traffic lights.
+    self.setContentSize(NSSize(width: 1400, height: 900))
+    self.styleMask.update(with: StyleMask.fullSizeContentView)
+    self.titleVisibility = TitleVisibility.hidden
+    self.titlebarAppearsTransparent = true
+    self.backgroundColor = NSColor(red: 0.162, green: 0.177, blue: 0.19, alpha: 1)
+    self.toolbar = NSToolbar()
+    self.toolbarStyle = ToolbarStyle.unifiedCompact
   }
 }
